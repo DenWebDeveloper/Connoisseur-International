702ccd8dbc3b16abb393e53c5b5280c7ee7ca7ca
Denys <denv928@gmail.com>
2017-11-27 17:49:23 +0200

    Видалений gulp task clean. Удалило весь проект. Github мене врятував

--- gulp/paths/tasks.js
+++ gulp/paths/tasks.js
@@ -5,7 +5,6 @@ module.exports = [
   './gulp/tasks/serve.js',
   './gulp/tasks/pug.js',
   './gulp/tasks/watch.js',
-  './gulp/tasks/clean.js',
   './gulp/tasks/js.foundation.js',
   './gulp/tasks/css.foundation.js',
   './gulp/tasks/js.process.js',
