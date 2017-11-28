2c0a5083e5f83189e229d65cd7fd78551a4e7041
Denys <denv928@gmail.com>
2017-11-29 00:33:01 +0200

    Зроблений блок About us

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,34 +1,29 @@
-var swiper = new Swiper('.swiper-container', {
-  effect: 'coverflow',
-   parallax: true,
+var swiper = new Swiper("#services .swiper-container", {
+  effect: "coverflow",
+  parallax: true,
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
     depth: 100,
     modifier: 1,
-    slideShadows : true,
+    slideShadows: true
   },
   pagination: {
-    el: '.swiper-pagination',
-  },
+    el: ".swiper-pagination"
+  }
 });
 
-/*var swiper = new Swiper('.swiper-container--second', {
-  effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows : true,
-  },
+var swiper = new Swiper("#about .swiper-container", {
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
+    type: "progressbar"
   },
-});*/
+  grabCursor: true,
+  slideToClickedSlide: true,
+  autoplay: {
+    delay: 8000
+  }
+});
