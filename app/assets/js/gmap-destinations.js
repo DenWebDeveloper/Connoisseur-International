16ab509324c451274005544a2edbffca018378dc
Denys <denv928@gmail.com>
2017-12-05 21:49:08 +0200

    Добалений масив координат

--- app/assets/js/gmap-destinations.js
+++ app/assets/js/gmap-destinations.js
@@ -2,29 +2,204 @@ var markersData = [
   {
     position: {
       lat: -37.843843, // Широта
-      lng: 144.760568 // Долгота
+      lng: 144.780981 // Долгота
     },
     name: "Australia",
     status: true,
-    pText: "<p>Connoisseur International Distribution Ltd C/O<br> Freight de Way Services PTE Ltd<br>115 Airport Road 08-04 Cargo Agents Bldg C<br>Changi Airfreight Centre<br>Singapore 819834<br>Tel: 006565432112<br>Fax: 006565431131<br>Email: operation@fdw.com.sg<br></p>"
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>Global Freight PTY Ltd.<br>119-121 William Angliss Drive<br>Laveton Victoria<br>3026<br>Australia<br>Tel: 0061393680600<br>Fax: 0061386307808<br></p>"
     },  
-  {
+   {
     position: {
-      lat: -27.843843, // Широта
-      lng: 0 // Долгота
+      lat: 49.179038, // Широта
+      lng: -123.161598 // Долгота
     },
     name: "Canada",
-    status: true // Произвольное название, которое будем выводить в информационном окне
-  },
-  {
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>Cargo Spectrum Forwarding<br>321, 5400 Airport Road South<br>Richmond British Columbia<br>V7B 1B4 Canada<br>Offices in Toronto & Montreal<br>Tel: 0016042735575<br>Fax: 0016042735579<br>Email: martinh@cargospectrum.com<br><p>"
+    },
+   {
     position: {
-      lat: 0, // Широта
-      lng: 144.760568 // Долгота
+      lat: 31.2118401, // Широта
+      lng: 121.4545044 // Долгота
     },
     name: "China",
-    status: true // Произвольное название, которое будем выводить в информационном окне
-  }
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>CLASQUIN SHANGHAI LTD<br>ROOM 203, QINGKE MANSION<br>NO. 138 FEN YANG ROAD<br>SHANGHAI 200031 P.R. OF CHINA<br>Tel: 00862164451452<br>Fax: 00862164457483<br>Email: sha@clasquin.com<br></p>"
+    },
+     {
+    position: {
+      lat: 49.5, // Широта
+      lng: 22 // Долгота
+    },
+    name: "Europe",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd<br>Unit 1 Beta Way<br>Thorpe Industrial Park<br>EGHAM<br>Surrey<br>TW20 8RE<br>Tel: +44 (0)1895 438822<br>Fax: +44 (0)1895 438833<br>Email: sales@cid.uk.com<br>Email: wine@cid.uk.com<br></p>"
+    },
+     {
+    position: {
+      lat: 51.417750, // Широта
+      lng: -0.528028 // Долгота
+    },
+    name: "Germany",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd<br>Unit 1 Beta Way<br>Thorpe Industrial Park<br>EGHAM<br>Surrey<br>TW20 8RE<br>Tel: +44 (0)1895 438822<br>Fax: +44 (0)1895 438833<br>Email: sales@cid.uk.com<br>Email: wine@cid.uk.com<br></p>"
+    },
+     {
+    position: {
+      lat: 51.417750, // Широта
+      lng: -0.528028 // Долгота
+    },
+    name: "France",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd<br>Unit 1 Beta Way<br>Thorpe Industrial Park<br>EGHAM<br>Surrey<br>TW20 8RE<br>Tel: +44 (0)1895 438822<br>Fax: +44 (0)1895 438833<br>Email: sales@cid.uk.com<br>Email: wine@cid.uk.com<br></p>"
+    },
+     {
+    position: {
+      lat: 22.321937, // Широта
+      lng: 114.208588 // Долгота
+    },
+    name: "Hong Kong",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>CLASQUIN FAR EAST LTD<br>UNITS 05-09, 12/F- METROLOFT<br>38 KWAI HEI STREET<br>KWAI CHUNG,NEW TERRITORIES<br>HONG KONG<br>Tel: 0085227587832<br>Fax: 0085227582855<br>Email: hkg@clasquin.com<br></p>"
+    },
+     {
+    position: {
+      lat: 21.13, // Широта
+      lng: 82.78 // Долгота
+    },
+    name: "India",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>Clasquin India PVT Ltd<br>Unit 4-5 Ground Floor<br>Palm Court Section 14<br>122001 Gurgaon<br>India<br>Tel: +91 (0) 124 402 4900<br>Fax: +91 (0) 124 402 4930<br>Email: del@clasquin.com<br></p>"
+    },
+     {
+    position: {
+      lat: 51.417750, // Широта
+      lng: -0.528028 // Долгота
+    },
+    name: "Japan",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd<br>Unit 1 Beta Way<br>Thorpe Industrial Park<br>EGHAM<br>Surrey<br>TW20 8RE<br>Tel: +44 (0)1895 438822<br>Fax: +44 (0)1895 438833<br>Email: sales@cid.uk.com<br>Email: wine@cid.uk.com<br></p>"
+    },
+     {
+    position: {
+      lat: 22.19282, // Широта
+      lng: 113.541413 // Долгота
+    },
+    name: "Macau",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>DHC Logistics (Macau) Ltd.<br>Avenida da Praia Grande No 619<br>Edificio Commercial Si Toi 7 Andar B<br>Macau<br>Tel: 0085328788063<br>Fax: 0085328788093<br>Email: pollywong@dhclogistics.com<br></p>"
+    },
+     {
+    position: {
+      lat: 3.2092858, // Широта
+      lng: 101.5731438 // Долгота
+    },
+    name: "Malaysia",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>Vins Vault Enterprise<br>7 Jalan Nautika B U20/B<br>Seksyen U20, Pusat Komersil TSB<br>40160 Sungai Buloh, Selangor<br>Phone: 0060123333977<br>Email: davidkschew@gmail.com<br></p>"
+    },
+     {
+    position: {
+      lat: -36.9065553, // Широта
+      lng: 174.9492629 // Долгота
+    },
+    name: "newZealand",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>Omega Customs & Forwarding<br>1st Floor 30 Springs Road<br>East Tamaki Auckland New Zealand<br>Tel: 006492651203<br>Fax: 006492651204<br>Email: greg@omegacustoms.co.nz<br></p>"
+    },
+     {
+    position: {
+      lat: 1.374325, // Широта
+      lng: 103.996108 // Долгота
+    },
+    name: "Singapore",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>Freight de Way Services PTE Ltd<br>115 Airport Road 08-04 Cargo Agents Bldg C<br>Changi Airfreight Centre<br>Singapore 819834<br>Tel: 006565432112<br>Fax: 006565431131<br>Email: operation@fdw.com.sg<br></p>"
+    },
+     {
+    position: {
+      lat: -33.89755, // Широта
+      lng: 18.6360199 // Долгота
+    },
+    name: "southAfrica",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>Logistic Business Partners SA(PTY)Ltd<br>LBP House, 31 Sarel Cilliers Street<br>Oakdale, Bellville<br>7532 South Africa<br>Tel: 0027219462281<br>Fax: 0027219462301<br>Email: Lindsay@lbp.co.za<br></p>"
+    },
+     {
+    position: {
+      lat: -21.74, // Широта
+      lng: -63.28 // Долгота
+    },
+    name: "Americas",
+    status: true,
+    pText: "<p>Representation in:</p><ul>    <li>Argentina</li>    <li>Brazil</li>    <li>Chile</li>    <li>Costa Rica</li>    <li>Falkland Islands</li>    <li>Guyana</li>    <li>Mexico</li>    <li>Panama</li>    <li>Peru</li>    <li>And other countries of Central and South America.</li></ul><p><b>Please contact us for more information.</b><br><br></p>"
+    },
+     {
+    position: {
+      lat: 40.407653, // Широта
+      lng: -3.6118774 // Долгота
+    },
+    name: "Spain",
+    status: true,
+    pText: "<div> <ul class=\"nav nav-tabs\"> <li class=\"nav-item\" ><a class=\"nav-link nav-link active\" href=\"#spain-contact-1\" data-toggle=\"tab\" aria-expanded=\"true\">Mainland & Canary Islands</a></li>  <li class=\"nav-item\" ><a class=\"nav-link nav-link \" href=\"#spain-contact-2\" data-toggle=\"tab\" aria-expanded=\"false\">Balearic Islands</a></li> </ul> <div id=\"myTabContent\" class=\"tab-content\"> <div class=\"tab-pane fade active in\" id=\"spain-contact-1\"> ER Transit<br>C/ Grabadores 1<br>Naves 4-5-6<br>Madrid (España)<br>Tel: (+34) 916 690 102<br>Fax: (+34) 916 690 008<br>Email: comercial.general@ertransit.com<br></div><div class=\"tab-pane fade\" id=\"spain-contact-2\">Rapid Transit<br>Passeig Maritimo 44<br>07015 Palma<br>Spain<br>Tel: +34 971 40 12 10<br>Email: bruno@rapidtrans.com<br></div></div></div>"
+    },
+     {
+    position: {
+      lat: 47.4869549, // Широта
+      lng: 7.5259561 // Долгота
+    },
+    name: "Switzerland",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>TRS International Transport & Shipping Ltd.<br>Erlenstrasse 27 B<br>P.O. Box 467<br>4106 Therwil<br>Tel: +41 (0) 61 726 94 00<br>Fax: +41 (0) 61 726 94 05<br>Email: mail@trs-shipping.ch<br></p"
+    },
+     {
+    position: {
+      lat: 25.0601727, // Широта
+      lng: 121.5593073 // Долгота
+    },
+    name: "Taiwan",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>Everhonest World Transport<br>9F-1 No343 Nanjing East Road<br>Taipei Taiwan<br>Tel: 00886227475877<br>Fax: 00886227477222<br>Email: therena@eveair.com.tw<br></p>"
+    },
+     {
+    position: {
+      lat: 40.844582, // Широта
+      lng: -73.712574 // Долгота
+    },
+    name: "caribbean",
+    status: true,
+    pText: "<p>Connoisseur International Distribution Ltd C/O<br>E Couriers<br>Unit 802 Fernandes Industrial Centre<br>Main East Road<br>Laventille Port of Spain<br>Trinidad & Tobago<br>Tel: 0018686262351<br>Email: sydney.martin@getezone.com<br></p>"
+    },
+     {
+    position: {
+      lat: 27.0821, // Широта
+      lng: 44.0150558 // Долгота
+    },
+    name: "middleEast",
+    status: true,
+    pText: " <div><p>Representation in:</p><ul>    <li>Bahrain</li>    <li>Doha</li>    <li>Egypt</li>    <li>Jordan</li>    <li>Lebanon</li>    <li>UAE</li></ul><p>And other countries of Middle East.</p><p>Please <a href=\"mailto:sales@cid.uk.com?Subject=Web Enquiries - The Middle East\"><i class=\"glyphicon glyphicon-envelope\" style=\"margin-right: 5px;\"></i>contact us</a> for more information.</p></div>"
+    },
+     {
+    position: {
+      lat: 51.417750, // Широта
+      lng: -0.528028 // Долгота
+    },
+    name: "unitedKingdom",
+    status: true,
+    pText: " <p>Connoisseur International Distribution Ltd<br>Unit 1 Beta Way<br>Thorpe Industrial Park<br>EGHAM<br>Surrey<br>TW20 8RE<br>Tel: +44 (0)1895 438822<br>Fax: +44 (0)1895 438833<br>Email: sales@cid.uk.com<br>Email: wine@cid.uk.com<br></p>"
+    },
+     {
+    position: {
+      lat: -40.84, // Широта
+      lng: -73.72 // Долгота
+    },
+    name: "usa",
+    status: true,
+    pText: " <p>CID USA<br>1 Pleasant Ave<br>STE G<br>Port Washington<br>NY 11050<br>Email: ops@cid-usa.com<br>Tel: 001516-805-8304<br></p>"
+    },
+
 ];
+
 
 var map, infoWindow;
 
